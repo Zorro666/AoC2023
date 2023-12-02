1e231b6604e3a49a502414373366a137da72e5ff
Jake Turner <jake@evansturner.co.uk>
2023-12-02 09:11:18 +0000

    Day02 done

--- test_day02.py
+++ test_day02.py
@@ -4,22 +4,22 @@ import unittest
 class Day02(unittest.TestCase):
     def test_part1(self):
         lines = """
-        1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
         """
-        self.assertEqual(day02.solvePart1(lines.splitlines()), 142)
+        self.assertEqual(day02.solvePart1(lines.splitlines()), 8)
     def test_part2(self):
-        lines = """two1nine
-        eightwothree
-        abcone2threexyz
-        xtwone3four
-        4nineeightseven2
-        zoneight234
-        7pqrstsixteen
+        lines = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
         """
-        self.assertEqual(day02.solvePart2(lines.splitlines()), 281)
+        self.assertEqual(day02.solvePart2(lines.splitlines()), 2286)
 
 if __name__ == '__main__':
     unittest.main()
