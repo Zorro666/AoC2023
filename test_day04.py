aa66329f88ff62c6b300d0870d454ca4438f502a
Jake Turner <jake@evansturner.co.uk>
2023-12-04 07:17:02 +0000

    Day04 done

--- test_day04.py
+++ test_day04.py
@@ -4,22 +4,24 @@ import unittest
 class Day04(unittest.TestCase):
     def test_part1(self):
         lines = """
-        1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet
-        """
-        self.assertEqual(day04.solvePart1(lines.splitlines()), 142)
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+        self.assertEqual(day04.solvePart1(lines.splitlines()), 13)
     def test_part2(self):
-        lines = """two1nine
-        eightwothree
-        abcone2threexyz
-        xtwone3four
-        4nineeightseven2
-        zoneight234
-        7pqrstsixteen
-        """
-        self.assertEqual(day04.solvePart2(lines.splitlines()), 281)
+        lines = """
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+        self.assertEqual(day04.solvePart2(lines.splitlines()), 30)
 
 if __name__ == '__main__':
     unittest.main()
