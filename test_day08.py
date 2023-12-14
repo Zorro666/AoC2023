38e8b5112045d5ded21e820545ac48b9902e16bd
Jake Turner <jake@evansturner.co.uk>
2023-12-14 19:36:35 +0000

    Day08 completed

--- test_day08.py
+++ test_day08.py
@@ -2,24 +2,42 @@ import day08
 import unittest
 
 class Day08(unittest.TestCase):
-    def test_part1(self):
+    def test_part1_1(self):
         lines = """
-        1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
         """
-        self.assertEqual(day08.solvePart1(lines.splitlines()), 142)
+        self.assertEqual(day08.solvePart1(lines.splitlines()), 2)
+    def test_part1_2(self):
+        lines = """
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+        """
+        self.assertEqual(day08.solvePart1(lines.splitlines()), 6)
     def test_part2(self):
-        lines = """two1nine
-        eightwothree
-        abcone2threexyz
-        xtwone3four
-        4nineeightseven2
-        zoneight234
-        7pqrstsixteen
+        lines = """
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
         """
-        self.assertEqual(day08.solvePart2(lines.splitlines()), 281)
+        self.assertEqual(day08.solvePart2(lines.splitlines()), 6)
 
 if __name__ == '__main__':
     unittest.main()
