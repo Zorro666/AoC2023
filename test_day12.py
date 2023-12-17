aa10437cf84be2bce116a55189cb2324c047910e
Jake Turner <jake@evansturner.co.uk>
2023-12-17 07:48:13 +0000

    Day12 Start

--- test_day12.py
+++ test_day12.py
@@ -4,20 +4,22 @@ import unittest
 class Day12(unittest.TestCase):
     def test_part1(self):
         lines = """
-        1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
         """
-        self.assertEqual(day12.solvePart1(lines.splitlines()), 142)
+        self.assertEqual(day12.solvePart1(lines.splitlines()), 21)
     def test_part2(self):
-        lines = """two1nine
-        eightwothree
-        abcone2threexyz
-        xtwone3four
-        4nineeightseven2
-        zoneight234
-        7pqrstsixteen
+        lines = """
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
         """
         self.assertEqual(day12.solvePart2(lines.splitlines()), 281)
 
