c875a6a9b54e9f79e8be21ab31340946c6285025
Jake Turner <jake@evansturner.co.uk>
2024-11-06 22:30:11 +0000

    Day17 Done

--- all_tests.py
+++ all_tests.py
@@ -1,6 +1,596 @@
+import day01
+import day02
+import day03
+import day04
+import day05
+import day06
+import day07
+import day08
+import day09
+import day10
+import day11
+import day12
+import day13
+import day14
+import day15
 import day16
+import day17
 import unittest
 
+class Day01(unittest.TestCase):
+    def test_part1(self):
+        lines = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+        """
+        self.assertEqual(day01.solvePart1(lines.splitlines()), 142)
+    def test_part2(self):
+        lines = """two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+        """
+        self.assertEqual(day01.solvePart2(lines.splitlines()), 281)
+
+class Day02(unittest.TestCase):
+    def test_part1(self):
+        lines = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """
+        self.assertEqual(day02.solvePart1(lines.splitlines()), 8)
+    def test_part2(self):
+        lines = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """
+        self.assertEqual(day02.solvePart2(lines.splitlines()), 2286)
+
+class Day03(unittest.TestCase):
+    def test_part1(self):
+        lines = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+        self.assertEqual(day03.solvePart1(lines.splitlines()), 4361)
+    def test_part2(self):
+        lines = """467+.114..
++..*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+        self.assertEqual(day03.solvePart2(lines.splitlines()), 467835)
+
+class Day04(unittest.TestCase):
+    def test_part1(self):
+        lines = """
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+        self.assertEqual(day04.solvePart1(lines.splitlines()), 13)
+    def test_part2(self):
+        lines = """
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+        self.assertEqual(day04.solvePart2(lines.splitlines()), 30)
+
+class Day05(unittest.TestCase):
+    lines = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+    def test_part1(self):
+        self.assertEqual(day05.solvePart1(self.lines.splitlines()), 35)
+    def test_part2(self):
+        self.assertEqual(day05.solvePart2(self.lines.splitlines()), 46)
+
+class Day06(unittest.TestCase):
+    lines = """
+Time:      7  15   30
+Distance:  9  40  200
+        """
+    def test_part1(self):
+        self.assertEqual(day06.solvePart1(self.lines.splitlines()), 288)
+    def test_part2(self):
+        self.assertEqual(day06.solvePart2(self.lines.splitlines()), 71503)
+
+class Day07(unittest.TestCase):
+    def test_part1(self):
+        lines = """
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+        """
+        self.assertEqual(day07.solvePart1(lines.splitlines()), 6440)
+    def test_rank_hand6(self):
+        self.assertEqual(day07.rank_hand("AAAAA"), 6)
+    def test_rank_hand5(self):
+        self.assertEqual(day07.rank_hand("9A999"), 5)
+    def test_rank_hand4(self):
+        self.assertEqual(day07.rank_hand("AA999"), 4)
+    def test_rank_hand3(self):
+        self.assertEqual(day07.rank_hand("83889"), 3)
+    def test_rank_hand2(self):
+        self.assertEqual(day07.rank_hand("32329"), 2)
+    def test_rank_hand1(self):
+        self.assertEqual(day07.rank_hand("K23K9"), 1)
+    def test_rank_hand0(self):
+        self.assertEqual(day07.rank_hand("AKQT9"), 0)
+    def test_hand_greater0(self):
+        h_i = "T55J5"
+        h_j = "QQQJA"
+        r_i = day07.rank_hand(h_i)
+        r_j = day07.rank_hand(h_j)
+        day07.prepare1()
+        self.assertEqual(day07.hand_greater(r_i, r_j, h_i, h_j), False)
+    def test_hand_greater1(self):
+        h_i = "QQQJA"
+        h_j = "T55J5"
+        r_i = day07.rank_hand(h_i)
+        r_j = day07.rank_hand(h_j)
+        day07.prepare1()
+        self.assertEqual(day07.hand_greater(r_i, r_j, h_i, h_j), True)
+    def test_hand_greater2(self):
+        h_i = "KK3KK"
+        h_j = "22522"
+        r_i = day07.rank_hand(h_i)
+        r_j = day07.rank_hand(h_j)
+        day07.prepare1()
+        self.assertEqual(day07.hand_greater(r_i, r_j, h_i, h_j), True)
+    def test_part2(self):
+        lines = """
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+        """
+        self.assertEqual(day07.solvePart2(lines.splitlines()), 5905)
+
+class Day08(unittest.TestCase):
+    def test_part1_1(self):
+        lines = """
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+        """
+        self.assertEqual(day08.solvePart1(lines.splitlines()), 2)
+    def test_part1_2(self):
+        lines = """
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+        """
+        self.assertEqual(day08.solvePart1(lines.splitlines()), 6)
+    def test_part2(self):
+        lines = """
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+        """
+        self.assertEqual(day08.solvePart2(lines.splitlines()), 6)
+
+class Day09(unittest.TestCase):
+    def test_part1(self):
+        lines = """
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+        """
+        self.assertEqual(day09.solvePart1(lines.splitlines()), 114)
+    def test_part2(self):
+        lines = """
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+        """
+        self.assertEqual(day09.solvePart2(lines.splitlines()), 2)
+class Day10(unittest.TestCase):
+    def test_part1_1(self):
+        lines = """
+.....
+.S-7.
+.|.|.
+.L-J.
+.....
+        """
+        self.assertEqual(day10.solvePart1(lines.splitlines()), 4)
+    def test_part1_2(self):
+        lines = """
+7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ
+        """
+        self.assertEqual(day10.solvePart1(lines.splitlines()), 8)
+    def test_part2_1(self):
+        lines = """
+.....
+.S-7.
+.|.|.
+.L-J.
+.....
+        """
+        self.assertEqual(day10.solvePart2(lines.splitlines()), 1)
+    def test_part2_2(self):
+        lines = """
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+        """
+        self.assertEqual(day10.solvePart2(lines.splitlines()), 4)
+    def test_part2_3(self):
+        lines = """
+..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........
+        """
+        self.assertEqual(day10.solvePart2(lines.splitlines()), 4)
+    def test_part2_4(self):
+        lines = """
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+        """
+        self.assertEqual(day10.solvePart2(lines.splitlines()), 8)
+    def test_part2_5(self):
+        lines = """
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+        """
+        self.assertEqual(day10.solvePart2(lines.splitlines()), 10)
+
+class Day11(unittest.TestCase):
+    def test_part1(self):
+        lines = """
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+        """
+        self.assertEqual(day11.solvePart1(lines.splitlines()), 374)
+    def test_part2_1(self):
+        lines = """
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+        """
+        self.assertEqual(day11.solve_it(lines.splitlines(), 10), 1030)
+    def test_part2_2(self):
+        lines = """
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+        """
+        self.assertEqual(day11.solve_it(lines.splitlines(), 100), 8410)
+
+class Day12(unittest.TestCase):
+    def test_part1(self):
+        lines = """
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+"""
+        self.assertEqual(day12.solvePart1(lines.splitlines()), 21)
+    def test_countMatches(self):
+        tests = [
+("#.#.### 1,1,3", 1),
+("???.### 1,1,3", 1),
+(".??..??...?##. 1,1,3", 4),
+("?#?#?#?#?#?#?#? 1,3,1,6", 1),
+("????.#...#... 4,1,1", 1),
+("????.######..#####. 1,6,5", 4),
+("?###???????? 3,2,1", 10),
+                ]
+        for test in tests:
+            self.assertEqual(day12.countMatches(test[0]), test[1])
+    def test_countMatchesUnFolded(self):
+        tests = [
+("???.### 1,1,3", 1),
+(".??..??...?##. 1,1,3", 16384),
+("?#?#?#?#?#?#?#? 1,3,1,6", 1),
+("????.#...#... 4,1,1", 16),
+("????.######..#####. 1,6,5", 2500),
+("?###???????? 3,2,1", 506250),
+                ]
+        for test in tests:
+            self.assertEqual(day12.countMatchesUnFolded(test[0]), test[1])
+    def test_part2(self):
+        lines = """
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+"""
+        self.assertEqual(day12.solvePart2(lines.splitlines()), 525152)
+
+class Day13(unittest.TestCase):
+    def test_pattern_score_1(self):
+        lines = """
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+"""
+        self.assertEqual(day13.pattern_score(lines.splitlines(), False), 5)
+    def test_pattern_score_2(self):
+        lines = """
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+"""
+        self.assertEqual(day13.pattern_score(lines.splitlines(), True), 100)
+    def test_part1(self):
+        lines = """
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+"""
+        self.assertEqual(day13.solvePart1(lines.splitlines()), 405)
+    def test_part2(self):
+        lines = """
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+"""
+        self.assertEqual(day13.solvePart2(lines.splitlines()), 400)
+class Day14(unittest.TestCase):
+    input = """
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+        """.splitlines()
+    def test_part1(self):
+        self.assertEqual(day14.solvePart1(self.input), 136)
+    def test_cycle1(self):
+        (grid, w, h) = day14.parse(self.input)
+        day14.runCycle(grid, w, h)
+        expected = """
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+"""
+        y = h - 1
+        for l in expected.splitlines():
+            l = l.strip()
+            if len(l) == 0:
+                continue
+            e = list(l)
+            self.assertEqual(grid[y], e)
+            y -= 1
+    def test_cycle2(self):
+        (grid, w, h) = day14.parse(self.input)
+        day14.runCycle(grid, w, h)
+        day14.runCycle(grid, w, h)
+        expected = """
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+"""
+        y = h - 1
+        for l in expected.splitlines():
+            l = l.strip()
+            if len(l) == 0:
+                continue
+            e = list(l)
+            self.assertEqual(grid[y], e)
+            y -= 1
+    def test_cycle3(self):
+        (grid, w, h) = day14.parse(self.input)
+        day14.runCycle(grid, w, h)
+        day14.runCycle(grid, w, h)
+        day14.runCycle(grid, w, h)
+        expected = """
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O
+"""
+        y = h - 1
+        for l in expected.splitlines():
+            l = l.strip()
+            if len(l) == 0:
+                continue
+            e = list(l)
+            self.assertEqual(grid[y], e)
+            y -= 1
+    def test_part2(self):
+        self.assertEqual(day14.solvePart2(self.input), 64)
+
+class Day15(unittest.TestCase):
+    input = """rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"""
+    def test_part1(self):
+        self.assertEqual(day15.solvePart1(self.input.splitlines()), 1320)
+    def test_part2(self):
+        self.assertEqual(day15.solvePart2(self.input.splitlines()), 145)
 class Day16(unittest.TestCase):
     def test_part1(self):
         lines = """
@@ -31,6 +621,27 @@ class Day16(unittest.TestCase):
         """
         self.assertEqual(day16.solvePart2(lines.splitlines()), 51)
 
+class Day17(unittest.TestCase):
+    lines = """
+2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533
+"""
+    def test_part1(self):
+        self.assertEqual(day17.solvePart1(self.lines.splitlines()), 102)
+
+    def test_part2(self):
+        self.assertEqual(day17.solvePart2(self.lines.splitlines()), 94)
 if __name__ == '__main__':
     unittest.main()
 
--- day17.py
+++ day17.py
@@ -1,22 +1,219 @@
 import aoc
+import queue
 
 """
+--- Day 17: Clumsy Crucible ---
+
+The lava starts flowing rapidly once the Lava Production Facility is operational.
+As you leave, the reindeer offers you a parachute, allowing you to quickly reach Gear Island.
+
+As you descend, your bird's-eye view of Gear Island reveals why you had trouble finding anyone on your way up: 
+half of Gear Island is empty, but the half below you is a giant factory city!
+
+You land near the gradually-filling pool of lava at the base of your new lavafall.
+Lavaducts will eventually carry the lava throughout the city, but to make use of it immediately, Elves are loading it into large crucibles on wheels.
+
+The crucibles are top-heavy and pushed by hand.
+Unfortunately, the crucibles become very difficult to steer at high speeds, and so it can be hard to go in a straight line for very long.
+
+To get Desert Island the machine parts it needs as soon as possible, you'll need to find the best way to get the crucible from the lava pool to the machine parts factory.
+To do this, you need to minimize heat loss while choosing a route that doesn't require the crucible to go in a straight line for too long.
+
+Fortunately, the Elves here have a map (your puzzle input) that uses traffic patterns, ambient temperature, and hundreds of other parameters to calculate exactly how much heat loss can be expected for a crucible entering any particular city block.
+
+For example:
+
+2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533
+
+Each city block is marked by a single digit that represents the amount of heat loss if the crucible enters that block.
+The starting point, the lava pool, is the top-left city block; the destination, the machine parts factory, is the bottom-right city block.
+(Because you already start in the top-left block, you don't incur that block's heat loss unless you leave that block and then return to it.)
+
+Because it is difficult to keep the top-heavy crucible going in a straight line for very long, it can move at most three blocks in a single direction before it must turn 90 degrees left or right.
+The crucible also can't reverse direction; after entering each city block, it may only turn left, continue straight, or turn right.
+
+One way to minimize heat loss is this path:
+
+2>>34^>>>1323
+32v>>>35v5623
+32552456v>>54
+3446585845v52
+4546657867v>6
+14385987984v4
+44578769877v6
+36378779796v>
+465496798688v
+456467998645v
+12246868655<v
+25465488877v5
+43226746555v>
+This path never moves more than three consecutive blocks in the same direction and incurs a heat loss of only 102.
+
+Directing the crucible from the lava pool to the machine parts factory, but not moving more than three consecutive blocks in the same direction, what is the least heat loss it can incur?
+
+-- Part Two ---
+The crucibles of lava simply aren't large enough to provide an adequate supply of lava to the machine parts factory.
+Instead, the Elves are going to upgrade to ultra crucibles.
+
+Ultra crucibles are even more difficult to steer than normal crucibles.
+Not only do they have trouble going in a straight line, but they also have trouble turning!
+
+Once an ultra crucible starts moving in a direction, it needs to move a minimum of four blocks in that direction before it can turn (or even before it can stop at the end).
+However, it will eventually start to get wobbly: an ultra crucible can move a maximum of ten consecutive blocks without turning.
+
+In the above example, an ultra crucible could follow this path to minimize heat loss:
+
+2>>>>>>>>1323
+32154535v5623
+32552456v4254
+34465858v5452
+45466578v>>>>
+143859879845v
+445787698776v
+363787797965v
+465496798688v
+456467998645v
+122468686556v
+254654888773v
+432267465553v
+In the above example, an ultra crucible would incur the minimum possible heat loss of 94.
+
+Here's another example:
+
+111111111111
+999999999991
+999999999991
+999999999991
+999999999991
+Sadly, an ultra crucible would need to take an unfortunate path like this one:
+
+1>>>>>>>1111
+9999999v9991
+9999999v9991
+9999999v9991
+9999999v>>>>
+This route causes the ultra crucible to incur the minimum possible heat loss of 71.
+
+Directing the ultra crucible from the lava pool to the machine parts factory, what is the least heat loss it can incur?
+
 """
+
+GO_LEFT = 0
+GO_RIGHT = 1
+GO_UP = 2
+GO_DOWN = 3
+
+h = 0
+w = 0
+grid = []
+visited: set[tuple[int,int,int,int]] = set()
+
+def parse(lines):
+    global h, w, grid, minHeatsPerDir
+    grid.clear()
+    visited.clear()
+    for l in lines:
+        l = l.strip()
+        if len(l) == 0:
+            continue
+        line = []
+        for c in l:
+            line.append(int(c))
+        grid.append(line)
+    h = len(grid)
+    w = len(grid[0])
+
+def solve(lines, min_steps, max_steps):
+    global h, w, grid, minHeats
+    parse(lines)
+
+    startingPoints = queue.PriorityQueue()
+    x = 0
+    y = 0
+    heat = 0
+    minHeat = 999999999
+    startingPoints.put((heat, x, y, GO_RIGHT, 0))
+    startingPoints.put((heat, x, y, GO_DOWN, 0))
+
+    while not startingPoints.empty():
+        current = startingPoints.get()
+        heat = current[0]
+        x = current[1]
+        y = current[2]
+        direction = current[3]
+        steps = current[4]
+        if x == w-1 and y == h-1 and steps >= min_steps:
+            if heat < minHeat:
+                print(f'New min heat: {heat}')
+                minHeat = heat
+            continue
+        if heat >= minHeat:
+            continue
+        new_visit = (x,y,direction,steps)
+        if new_visit in visited:
+            continue
+        visited.add(new_visit)
+
+#        print(f'x: {x}, y: {y}, heat: {heat}, direction: {direction}, steps: {steps}')
+        # Move one forward
+        # Rotate Left and move one
+        # Rotate Right and move one
+        new_moves = []
+        if direction == GO_LEFT:
+            new_moves.append((GO_LEFT, x-1, y, steps+1))
+            if steps >= min_steps:
+                new_moves.append((GO_UP, x, y-1, 1))
+                new_moves.append((GO_DOWN, x, y+1, 1))
+        elif direction == GO_RIGHT:
+            new_moves.append((GO_RIGHT, x+1, y, steps+1))
+            if steps >= min_steps:
+                new_moves.append((GO_UP, x, y-1, 1))
+                new_moves.append((GO_DOWN, x, y+1, 1))
+        elif direction == GO_UP:
+            new_moves.append((GO_UP, x, y-1, steps+1))
+            if steps >= min_steps:
+                new_moves.append((GO_LEFT, x-1, y, 1))
+                new_moves.append((GO_RIGHT, x+1, y, 1))
+        elif direction == GO_DOWN:
+            new_moves.append((GO_DOWN, x, y+1, steps+1))
+            if steps >= min_steps:
+                new_moves.append((GO_LEFT, x-1, y, 1))
+                new_moves.append((GO_RIGHT, x+1, y, 1))
+        
+        for move in new_moves:
+            new_x = move[1]
+            new_y = move[2]
+            if new_x < 0 or new_x >= w or new_y < 0 or new_y >= h:
+                continue
+            new_steps = move[3]
+            if new_steps > max_steps:
+                continue
+            new_direction = move[0]
+            new_heat = heat + grid[new_y][new_x]
+            startingPoints.put((new_heat, new_x, new_y, new_direction, new_steps))
+
+    return minHeat
 
 def solvePart1(lines):
-    result = 0
-    for l in lines:
-        result += len(l)
-    return result
+    return solve(lines, 0, 3)
 
 def solvePart2(lines):
-    result = 0
-    for l in lines:
-        result += len(l)
-    return result
+    return solve(lines, 4, 10)
 
 def main():
-    aoc.run_day(17, solvePart1, 123, solvePart2, 456)
+    aoc.run_day(17, solvePart1, 665, solvePart2, 809)
 
 if __name__ == '__main__':
     main()
